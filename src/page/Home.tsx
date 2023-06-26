49526ddb0077de996218eb4cecdb5c1719a32157
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2023-06-26 17:17:42 +0200

    reorganisation composants

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Nav from "../componentsApp/Nav";
 import Main from "../componentHomePage/Main";
+import Footer from "../componentsApp/Footer";
 
 function Home() {
-  return <Main />;
+  return (
+    <body>
+      <Nav />
+      <Main />
+      <Footer />
+    </body>
+  );
 }
 
 export default Home;
--- src/page/Profil.tsx
+++ src/page/Profil.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Nav from "../componentsApp/Nav";
+import Footer from "../componentsApp/Footer";
 import Main from "../componentProfilPage/Main";
 
 function Profil() {
-  return <Main />;
+  return (
+    <body>
+      <Nav />
+      <Main />
+      <Footer />
+    </body>
+  );
 }
 
 export default Profil;
