67b89812182d3cf7c7eb691c95dec8e2461f369b
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2023-06-26 16:09:58 +0200

    réinstallation typescript, + mep page home

---
+++ src/componentsApp/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../componentHomePage/HeroSection";
+import Features from "../componentHomePage/Features";
+
+function Main() {
+  return (
+    <div>
+      <HeroSection />
+      <Features />
+    </div>
+  );
+}
+
+export default Main;
