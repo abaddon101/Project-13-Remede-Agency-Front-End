d56270c98d4d06354b0fb9df5f89ad1e8aa3a523
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2023-07-26 10:57:43 +0200

    modif composant profil page

--- src/componentProfilPage/Main.tsx
+++ src/componentProfilPage/Main.tsx
@@ -1,13 +1,46 @@
 import React from "react";
-import HeaderProfil from "./HeaderProfil";
-import AccountProfilPage from "./AccountProfilPage";
 
 function Main() {
-  
   return (
     <main className="main bg-dark">
-      <HeaderProfil />
-      <AccountProfilPage />
+      <div className="header">
+        <h1>
+          Welcome back
+          <br />
+        </h1>
+        <button className="edit-button">Edit Name</button>
+
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+            <p className="account-amount">$2,082.79</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+            <p className="account-amount">$10,928.42</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+            <p className="account-amount">$184.30</p>
+            <p className="account-amount-description">Current Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
